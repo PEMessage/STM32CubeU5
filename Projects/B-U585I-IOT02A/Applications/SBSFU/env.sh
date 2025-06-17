3fc47fcac4eb8e5fcb71b56928a1b00006fbdb8b
PEMessage <1165739182@qq.com>
2025-06-17 11:34:11 +0800

    change to env_linux.sh to pass SBSFU_Appli/STM32CubeIDE/postbuild.sh check
    Detail:
        you could run this standalone in shell after build
        ```
        cd SBSFU_Appli/STM32CubeIDE/NonSecure/Release &&
        ../../postbuild.sh . 1.0.0+0 nonsecure
            # arg1 is the build directory
            # arg2 is the version
            # arg3 is the binary type (1 nonsecure, 2 secure)
        ```

--- Projects/B-U585I-IOT02A/Applications/SBSFU/env.sh
+++ Projects/B-U585I-IOT02A/Applications/SBSFU/env.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
-stm32tpccli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32TrustedPackageCreator_CLI.exe"
-imgtool="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\Utilities\Windows\imgtool.exe"
+env_linux.sh
---
+++ Projects/B-U585I-IOT02A/Applications/SBSFU/env_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CUBE_PROGRAM_ROOT="$HOME/STMicroelectronics/STM32Cube/STM32CubeProgrammer"
+
+# stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
+stm32programmercli="$CUBE_PROGRAM_ROOT/bin/STM32_Programmer_CLI"
+
+# stm32tpccli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32TrustedPackageCreator_CLI.exe"
+stm32tpccli="$CUBE_PROGRAM_ROOT/bin/STM32TrustedPackageCreator_CLI"
+
+# imgtool="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\Utilities\Windows\imgtool.exe"
+imgtool="$CUBE_PROGRAM_ROOT/bin/Utilities/Linux/imgtool"
