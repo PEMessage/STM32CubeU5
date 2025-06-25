6e2e727ec098a590138088fabedb03cfeb0fd47e
PEMessage <1165739182@qq.com>
2025-06-25 14:23:14 +0800

    add some comment on how to run imgtool from source
    Detail:
        cd stm32-mw-mcuboot
        cd scripts
        uv venv
        uv pip install pudb
        source .venv/bin/activate # seem like uv run have wired behavior

--- Projects/B-U585I-IOT02A/Applications/SBSFU/env_linux.sh
+++ Projects/B-U585I-IOT02A/Applications/SBSFU/env_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# source ~/stm32-mw-mcuboot/scripts/.venv/bin/activate # # for runing from source
 CUBE_PROGRAM_ROOT="$HOME/STMicroelectronics/STM32Cube/STM32CubeProgrammer"
 
 # stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
@@ -9,4 +9,5 @@ stm32programmercli="$CUBE_PROGRAM_ROOT/bin/STM32_Programmer_CLI"
 stm32tpccli="$CUBE_PROGRAM_ROOT/bin/STM32TrustedPackageCreator_CLI"
 
 # imgtool="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\Utilities\Windows\imgtool.exe"
+# imgtool="~/stm32-mw-mcuboot/scripts/imgtool.py" # for runing from source
 imgtool="$CUBE_PROGRAM_ROOT/bin/Utilities/Linux/imgtool"
